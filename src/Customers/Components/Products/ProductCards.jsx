8fb733f5165702971910fd452b5a7be8a5054cfe
Nikhil <nikhilkalakkara05@gmail.com>
2023-11-13 14:52:37 +0530

    changed product methods second

--- src/Customers/Components/Products/ProductCards.jsx
+++ src/Customers/Components/Products/ProductCards.jsx
@@ -9,32 +9,32 @@ const ProductCards=({ product })=> {
  
   return (
     <>
-    { product.map((item)=><div onClick={() => {
+    <div onClick={() => {
       console.log("Click event fired!");
-       navigate(`/product/${item.id}`);
-        }}
+       navigate(`/product/${product.id}`);
+    }}
      className="productCard w-[15rem] m-3 transition-all cursor-pointer">
       <div  className="h-[20rem]">
         <img 
           className="h-full w-full object-cover object-left-top "
-          src={item.imageUrl}
+          src={product.imageUrl}
         />
       </div>
 
       <div className="textPart bg-white p-3">
         <div>
-          <p className="font-bold opacity-60">{item.brand}</p>
-          <p>  {item.title}</p>
+          <p className="font-bold opacity-60">{product.brand}</p>
+          <p>  {product.title}</p>
         </div>
         <div className="flex items-center space-x-2 ">
-          <p className="font-semibold">₹{item.discountPrice}</p>
-          <p className="line-through opacity-50">{item.price}</p>
-          <p className="text-green-600 font-semibold">{item.discountPercent}% off</p>
+          <p className="font-semibold">₹{product.discountPrice
+}</p>
+          <p className="line-through opacity-50">{product.price}</p>
+          <p className="text-green-600 font-semibold">{product.discountPercent}% off</p>
         </div>
       </div>
-    </div>)}
-
-    </> 
+    </div>
+    </>
   );
 }
 
